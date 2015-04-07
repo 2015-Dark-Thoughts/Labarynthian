9fa1d9990dd3e0934779c5901292291cc705d2d7
ConnerCrabtree <ilovesoccer127@Conners-MBP.brentwood.bc.ca>
2015-04-07 10:52:34 -0700

    Initial Check In

---
+++ src/labarynthian/LabarynthianEnvironment.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labarynthian;
+import environment.Environment;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import audio.AudioPlayer;
+import environment.GraphicsPalette;
+import environment.LocationValidatorIntf;
+import grid.Grid;
+import images.ResourceTools;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author ilovesoccer127
+ */
+public class LabarynthianEnvironment extends Environment {
+
+    public LabarynthianEnvironment() {
+
+    }
+
+    @Override
+    public void initializeEnvironment() {
+
+    }
+
+    @Override
+    public void timerTaskHandler() {
+
+    }
+
+    @Override
+    public void keyPressedHandler(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleasedHandler(KeyEvent e) {
+
+    }
+
+    @Override
+    public void environmentMouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void paintEnvironment(Graphics graphics) {
+
+    }
+    
+}
