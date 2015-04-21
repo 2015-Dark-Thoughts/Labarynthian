c30d2817b8b80fb807eb8babe43a56d9b6864c8c
Kevin Lawrence <kevinlawrence@outlook.com>
2015-04-21 05:39:27 -0700

    fixed room drawing

--- src/labarynthian/LabarynthianEnvironment.java
+++ src/labarynthian/LabarynthianEnvironment.java
@@ -43,11 +43,6 @@ class LabarynthianEnvironment extends Environment implements LocationValidatorIn
         setGameState(GameState.STARTMENU);
 
         character = new Number98(new Point(5, 5), this, this);
-
-        
-
-       
-
     }
 
     @Override
@@ -100,24 +95,27 @@ class LabarynthianEnvironment extends Environment implements LocationValidatorIn
                 case STARTROOM:
                     grid.setColumns(10);
                     grid.setRows(10);
-                    grid.setColor(Color.WHITE);
+                    grid.setColor(Color.LIGHT_GRAY);
                     grid.paintComponent(graphics);
                     grid.setPosition(new Point((getWidth() / 2) - 100, (getHeight() / 2) - 10));
-
-                   
                     
 
-                    graphics.fillRect(593, 417, 100, 100);
+//                    graphics.fillRect(593, 417, 100, 100);
+                    int border = 5;
+                    graphics.fillRect(grid.getPosition().x - border, grid.getPosition().y - border, grid.getGridSize().width + (2 * border), grid.getGridSize().height  + (2 * border));
 
                     graphics.setColor(Color.BLACK);
-                    graphics.fillRect(628, 417, 30, 10);
-                    graphics.fillRect(599, 422, 90, 90);
+//                    graphics.fillRect(628, 417, 30, 10);
+                    graphics.fillRect(grid.getPosition().x, grid.getPosition().y, grid.getGridSize().width, grid.getGridSize().height);
+//                    graphics.fillRect(599, 422, 90, 90);
+                    graphics.fillRect(grid.getPosition().x + (3 * grid.getCellWidth()) , grid.getPosition().y - border, grid.getGridSize().width - (6 * grid.getCellWidth()), 2 * border);
+//                    graphics.fillRect(599, 422, 90, 90);
                     
+                    grid.paintComponent(graphics);
+
                     if (character != null){
                         character.draw(graphics);
                     }
-                    
-                    grid.paintComponent(graphics);
 
                     break;
 
@@ -203,7 +201,8 @@ class LabarynthianEnvironment extends Environment implements LocationValidatorIn
 
     @Override
     public Point getCellSystemCorrdinate(Point cellCorrdinate) {
-        return grid.getCellLocationFromSystemCoordinate(cellCorrdinate);
+//        return grid.getCellLocationFromSystemCoordinate(cellCorrdinate);
+        return grid.getCellSystemCoordinate(cellCorrdinate);
     }
 
 //<editor-fold defaultstate="collapsed" desc="LocationValidatorIntf">
