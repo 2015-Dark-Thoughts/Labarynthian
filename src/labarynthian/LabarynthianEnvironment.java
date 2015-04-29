29ed37ab98fae5881b271c9eee823edbacc6a07f
ConnerCrabtree <ConnerCrabtree@Conners-MBP.brentwood.bc.ca>
2015-04-29 13:08:16 -0700

    Made the candle glow
    Made the candle glow

--- src/labarynthian/LabarynthianEnvironment.java
+++ src/labarynthian/LabarynthianEnvironment.java
@@ -28,6 +28,8 @@ class LabarynthianEnvironment extends Environment implements LocationValidatorIn
     private Image title;
     private GameState gameState = GameState.STARTMENU;
     private Number98 character;
+    
+    private int counter = 0;
 
     private boolean showGrid = false;
 
@@ -50,16 +52,16 @@ class LabarynthianEnvironment extends Environment implements LocationValidatorIn
 
     @Override
     public void timerTaskHandler() {
-
+        if (counter <= 200) {
+            counter++;
+        } else {
+            counter = 80;
+        }
+        character.setGlowInt((int) counter / 2);
     }
 
     @Override
     public void keyPressedHandler(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleasedHandler(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_1) {
             gameState = GameState.STARTROOM;
         } else if (e.getKeyCode() == KeyEvent.VK_2) {
@@ -88,21 +90,26 @@ class LabarynthianEnvironment extends Environment implements LocationValidatorIn
             if (character != null) {
                 character.move(Direction.UP);
             }
-        }
-            else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            if (character != null){
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            if (character != null) {
                 character.move(Direction.DOWN);
             }
         } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            if (character != null){
+            if (character != null) {
                 character.move(Direction.LEFT);
             }
         } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            if (character != null){
+            if (character != null) {
                 character.move(Direction.RIGHT);
             }
         }
 
+
+    }
+
+    @Override
+    public void keyReleasedHandler(KeyEvent e) {
+        
     }
 
     @Override
@@ -121,12 +128,12 @@ class LabarynthianEnvironment extends Environment implements LocationValidatorIn
                     grid.setRows(getHeight());
                     grid.setColor(Color.BLACK);
                     grid.paintComponent(graphics);
-                    
+
                     graphics.drawImage(getTitle(), 400, 100, null);
-                    
+
                     graphics.setColor(Color.WHITE);
                     graphics.drawString("PRESS SPACE TO BEGIN", (getWidth() / 2) - 50, getHeight() / 2);
-                    
+
                     break;
 //</editor-fold>
 
@@ -137,83 +144,87 @@ class LabarynthianEnvironment extends Environment implements LocationValidatorIn
                     grid.setColor(Color.LIGHT_GRAY);
                     grid.paintComponent(graphics);
                     grid.setPosition(new Point((getWidth() / 2) - 100, (getHeight() / 2) - 10));
-                    
+
 //                    graphics.fillRect(593, 417, 100, 100);
                     int border = 5;
                     graphics.fillRect(grid.getPosition().x - border, grid.getPosition().y - border, 100 + (2 * border), 100 + (2 * border));
-                    
+
                     graphics.setColor(Color.BLACK);
 //                    graphics.fillRect(628, 417, 30, 10);
                     graphics.fillRect(grid.getPosition().x, grid.getPosition().y, 100, 100);
 //                    graphics.fillRect(599, 422, 90, 90);
                     graphics.fillRect(grid.getPosition().x + (3 * grid.getCellWidth()), grid.getPosition().y - border, 100 - (6 * grid.getCellWidth()), 2 * border);
 //                    graphics.fillRect(599, 422, 90, 90);
-                    
+
                     if (showGrid) {
                         grid.paintComponent(graphics);
                     }
-                    
+
                     if (character != null) {
                         character.draw(graphics);
                     }
-                    
+                    if (grid.getPosition() == grid.getCellLocationFromSystemCoordinate(1, 5)) {
+                        gameState = GameState.MAZESTART;
+                    }
+
                     break;
 //</editor-fold>
 
                 //<editor-fold defaultstate="collapsed" desc="MAZESTART">
                 case MAZESTART:
-                    
+
                     break;
 //</editor-fold>
 
                 //<editor-fold defaultstate="collapsed" desc="MAZEEXPLORE">
                 case MAZEEXPLORE:
-                    
+
                     break;
 //</editor-fold>
 
                 //<editor-fold defaultstate="collapsed" desc="OASIS">
                 case OASIS:
-                    
+
                     break;
 //</editor-fold>
 
                 //<editor-fold defaultstate="collapsed" desc="TREEROOM">
                 case TREEROOM:
-                    
+
                     break;
 //</editor-fold>
 
                 //<editor-fold defaultstate="collapsed" desc="BOSSROOM">
                 case BOSSROOM:
-                    
+
                     break;
 //</editor-fold>
 
                 //<editor-fold defaultstate="collapsed" desc="PAUSED">
                 case PAUSED:
-                    
+
                     break;
 //</editor-fold>
 
                 //<editor-fold defaultstate="collapsed" desc="BATTLE">
                 case BATTLE:
-                    
+
                     break;
 //</editor-fold>
 
                 //<editor-fold defaultstate="collapsed" desc="DEAD">
                 case DEAD:
-                    
+
                     break;
 //</editor-fold>
 
                 //<editor-fold defaultstate="collapsed" desc="END">
                 case END:
-                    
+
                     break;
 //</editor-fold>
-
+                
+                    
             }
 
         }
@@ -285,4 +296,6 @@ class LabarynthianEnvironment extends Environment implements LocationValidatorIn
         return point;
     }
 //</editor-fold>
+
+
 }
